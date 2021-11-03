317237f9da9d0fa321080681b3352ba8ac82620f
Shriyans2023 <89887044+Shriyans2023@users.noreply.github.com>
2021-11-02 20:08:12 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+noseX=0;
+noseY=0;
+
 function preload ()
 {
+    clown_nose = loadImage('https://i.postimg.cc/Qtq2j6YK/mustache.png');
 }
     function setup() {
         canvas = createCanvas(300,300);
@@ -14,6 +18,10 @@ function preload ()
 
     function draw() {
         image(video, 0 , 0, 300, 300);
+        fill(255,0,0)
+        stroke(255,0,0)
+        circle(noseX, noseY, 20);
+        image(clown_nose, noseX, noseY, 30, 30);
     }
 
 
@@ -30,7 +38,9 @@ function preload ()
         if(results.length > 0)
         {
         console.log(results);
-        console.log("nose x = " + results[0] .pose.nose.x);
-        console.log("nose y = " + results[0] .pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
         }
     }
